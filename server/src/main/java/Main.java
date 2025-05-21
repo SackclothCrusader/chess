d46fdcf0144308227f1c73e232fd15bab072a537
SackclothCrusader <acegamerdragon@gmail.com>
2025-05-20 21:03:02 -0700

    setup web site

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Server: " + piece);
+
+        Spark.port(8080);
+        Spark.staticFiles.location("web");
+
+
+        Spark.get("/hello/:name", (req, res) -> { return "Hello " + req.params(":name");});
     }
 }
