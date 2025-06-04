53fd06af63371604588bd6babc54135cfa0b03c5
SackclothCrusader <acegamerdragon@gmail.com>
2025-06-03 19:42:58 -0700

    remove diagnostic print statement

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -24,7 +24,6 @@ public class DatabaseManager {
         try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
              var preparedStatement = conn.prepareStatement(statement)) {
             preparedStatement.executeUpdate();
-            System.out.println("Created Database!");
         } catch (SQLException ex) {
             throw new DataAccessException("failed to create database", ex);
         }
