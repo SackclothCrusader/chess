1debdff3e98d15e2cb1edddb9d585056dd8b84ab
SackclothCrusader <acegamerdragon@gmail.com>
2025-06-04 19:59:19 -0700

    fix something

--- shared/src/test/java/passoff/chess/extracredit/EnPassantTests.java
+++ shared/src/test/java/passoff/chess/extracredit/EnPassantTests.java
@@ -1,7 +1,7 @@
 package passoff.chess.extracredit;
 
 import chess.*;
-import exceptions.InvalidMoveException;
+import chess.InvalidMoveException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
