5b3019ebe4465441aac72d4f87e99fa3f1dc086d
SackclothCrusader <acegamerdragon@gmail.com>
2025-06-04 18:19:18 -0700

    begin repl

---
+++ client/src/main/java/client/LoginClient.java
@@ -0,0 +1,15 @@
+package client;
+
+public class LoginClient {
+
+    private String help() {
+        return """
+                - register <username> <password> <email>
+                - login <username> <password>
+                - quit
+               """;
+    }
+    private String login(String user, String password) {
+
+    }
+}
